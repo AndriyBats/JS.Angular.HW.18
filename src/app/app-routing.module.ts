cb29f56dd0bed5fbe98ac6ea64dbd03424c4c113
Andriy Bats <abats333@gmail.com>
2021-10-10 00:50:57 +0300

    add homework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { AdminBlogComponent } from './admin/admin-blog/admin-blog.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCategoryComponent } from './admin/admin-category/admin-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blog', component: BlogComponent},
+  {path: 'admin', component: AdminComponent, children: [
+    {path: '', pathMatch: 'full', redirectTo: 'admin-category'},
+    {path: 'admin-category', component: AdminCategoryComponent},
+    {path: 'admin-blog', component: AdminBlogComponent}
+  ]},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { AdminBlogComponent } from './admin/admin-blog/admin-blog.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCategoryComponent } from './admin/admin-category/admin-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    AdminBlogComponent,
+    HeaderComponent,
+    AdminComponent,
+    AdminCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
